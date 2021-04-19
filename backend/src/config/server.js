ab073c5d999d0f25342ef6c2a9072b995680fab0
marcelo.faria <faria.marcelofilho@gmail.com>
2021-04-18 21:36:52 -0300

    add cors, add list, test post, start delete

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,6 +1,9 @@
 // cria uma variável para definir a porta do servidor
 const port = 3003
 
+// importa o cors que aceita as requisições
+const allowCors = require('./cors')
+
 // importa a biblioteca body-parser
 const bodyParser = require('body-parser')
 
@@ -13,6 +16,9 @@ const server = express()
 // faremos o servidor oferecer suporte a body-parser
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
+
+// servidor utiliza a configuração do cors
+server.use(allowCors)
 
 // coloca o servidor para ficar ouvindo .....
 server.listen(port, function() {
--- frontend/front/src/TarefaForm.jsx
+++ frontend/front/src/TarefaForm.jsx
@@ -4,13 +4,42 @@ import axios from 'axios'
 export default class TarefaForm extends Component {
 
     constructor(){
+
         super()
+
         this.state = {
             descricao: "",
+            realizada: false,
+            criadaEm: undefined,
             quem: "",
             onde: "",
-            prioridade: 0
+            prioridade: 0,
+            lista: []
         }
+
+        this.buscaTarefas()
+
+    }
+
+    buscaTarefas(){
+        axios.get(`http://localhost:3003/api/tarefas`).then(
+            resp => this.setState({lista: resp.data})
+        )
+    }
+
+    criaLinhasTabela(){
+        return (
+            this.state.lista.map( cadaTarefa => (
+                <tr key={cadaTarefa._id}>
+                    <td>{cadaTarefa.descricao}</td>
+                    <td>{cadaTarefa.criadaEm}</td>
+                    <td>{cadaTarefa.quem}</td>
+                    <td>{cadaTarefa.onde}</td>
+                    <td>{cadaTarefa.prioridade}</td>
+                    <td><button type="button" onClick={ e => this.remover()} className="btn btn-danger">Remover</button></td>
+                </tr>
+            ))
+        )
     }
 
     setDescricao(e){
@@ -38,20 +67,28 @@ export default class TarefaForm extends Component {
 
         const newTask = {
             descricao: this.state.descricao,
+            realizada: this.state.realizada,
+            criadaEm: this.state.criadaEm,
             quem: this.state.quem,
             onde: this.state.onde,
             prioridade: this.state.prioridade
         }
 
         axios.post(
-            `https://localhost:3003/api/tarefas`, newTask
+            `http://localhost:3003/api/tarefas`, newTask
         ).then(resposta => console.log(`Funcionou ${resposta.data}`))
 
     }
 
+    remover(){
+        axios.delete(
+            `http://localhost:3003/api/tarefas`
+        )
+    }
+
     render(){
         return (
-            <form>
+            <div className="form">
                 <div className="form-group">
                     <label htmlFor="descricao"> Descrição </label>
                     <input className="form-control" onChange={e => this.setDescricao(e) } type="text" id="descricao" value={this.state.descricao}/>
@@ -71,7 +108,27 @@ export default class TarefaForm extends Component {
                 <div className="form-group">
                     <button type="button" onClick={ e => this.cadastrar() } className="btn btn-default"> Cadastro </button>
                 </div>
-            </form>
+                <div>
+                    <hr></hr>
+                    <h3>Lista de Tarefas</h3>
+                    <hr></hr>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th> Descrição </th>
+                                <th> Criada Em </th>
+                                <th> Quem </th>
+                                <th> Onde </th>
+                                <th> Prioridade </th>
+                                <th> Ações </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.criaLinhasTabela()}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         )
     }
 
